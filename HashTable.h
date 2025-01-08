b1fd881306ab5b0db60d4b47fb60826461ee8210
suchithgali <sgali@ucmerced.edu>
2025-01-08 12:41:47 -0800

    fixed issue where HasTable as unable to be printed due to issue with << operator overloading

--- HashTable.h
+++ HashTable.h
@@ -1,8 +1,17 @@
 #ifndef HASH_TABLE
 #define HASH_TABLE
 
+#include "ArrayList.h"
 #include "LinkedList.h"
 #include <string>
+#include <ostream>
+#include <typeinfo>
+
+template <class T>
+class HashTable;
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const HashTable<T>& container);
 
 template <class T>
 class HashTable{
@@ -10,6 +19,14 @@ class HashTable{
     ArrayList<LinkedList<T>> data;
 
     HashTable(){}
+
+public:
+    HashTable(int buckets){
+        this->buckets = buckets;
+        for (int i = 0; i < buckets; i++){
+            data.append(LinkedList<T>());
+        }
+    }
 
     int HashInts(int val){
         return val % buckets;
@@ -20,21 +37,34 @@ class HashTable{
         for (int i = 0; i < val.length(); i++){
             sum += static_cast<int>(val[i]);
         }
-        return sum;
+        return sum % buckets;
     }
 
-public:
-    HashTable(int buckets){
-        this->buckets = buckets;
-        for (int i = 0; i < buckets; i++){
-            data.append(LinkedList<T>());
-        }
+    void appendInt(int val){
+        int hashCode = 0;
+        hashCode = HashInts(val);
+        data[hashCode].appendinLL(val);
     }
+
+    void appendString(std::string val_string){
+        int hashCode = 0;
+        hashCode = HashStrings(val_string);
+        data[hashCode].appendinLL(val_string);
+    }
+
+    friend std::ostream& operator<< <>(std::ostream& os, const HashTable<T>& table);
 };
 
+template <class T>
+std::ostream& operator<<(std::ostream& os, const HashTable<T>& table){
+    // So we can cout the table
 
-
-
+    for (int i = 0; i < table.buckets; i++){
+        os<< i << ": " << table.data[i] << std::endl;
+    }
+    
+    return os;
+}
 
 #endif
 
